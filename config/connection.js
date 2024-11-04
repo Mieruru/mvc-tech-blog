ab2a4c10cda5f8eb2f703d57ce1613cd17b37cd4
Cassidy Pagels <rozeneclipse@gmail.com>
2024-11-04 04:31:37 -0600

    bugfix connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,14 @@ let sequelize
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL)
 } else {
-
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+  })
 }
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
-})
 
 // Export the sequelize instance
 module.exports = sequelize
