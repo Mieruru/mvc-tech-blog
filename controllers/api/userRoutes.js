ff6a1e9632cfb949c2f663fd8de4dd368be10d94
Cassidy Pagels <rozeneclipse@gmail.com>
2024-11-03 23:59:08 -0600

    build controllers, auth

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const { User } = require('../../models')
+
+router.post('/', async (req, res) => {
+
+})
+
+router.post('/login', async (req, res) => {
+
+})
+
+router.post('logout', async (req, res) => {
+
+})
+
+module.exports = router
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const { User, Post } = require('../models')
+const withAuth = require('../utils/auth')
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get('/login', async (req, res) => {
+
+})
+
+module.exports = router
